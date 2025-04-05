06ba75452cb3608e34f1691578b55a0eb4004c75
Giorgi Otinashvili <130919363+Otina12@users.noreply.github.com>
2025-04-06 00:22:11 +0400

    Time: 0 ms (100.00%) | Memory: 3.8 MB (74.47%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.go
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.go
@@ -1,20 +1,9 @@
-var _res int
-var _nums []int
-
 func subsetXORSum(nums []int) int {
-    _res = 0
-    _nums = nums
+    var res int = 0
 
-    helper(0, 0)
-    return _res
-}
-
-func helper(i int, cur_xor int) {
-    if i == len(_nums) {
-        _res += cur_xor
-        return
+    for _, num := range nums {
+        res |= num
     }
 
-    helper(i + 1, cur_xor)
-    helper(i + 1, cur_xor ^ _nums[i])
+    return res << (len(nums) - 1)
 }
