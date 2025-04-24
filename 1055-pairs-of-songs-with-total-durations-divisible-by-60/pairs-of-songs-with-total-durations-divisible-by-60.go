f23d8ac9f1e70122c245096a9c649754c203ee0e
Giorgi Otinashvili <130919363+Otina12@users.noreply.github.com>
2025-04-24 16:35:18 +0400

    Time: 15 ms (7.50%) | Memory: 8.5 MB (100.00%) - LeetSync

---
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.go
@@ -0,0 +1,20 @@
+func numPairsDivisibleBy60(time []int) int {
+    remainderFreq := make(map[int]int64)
+    var res int64 = 0
+
+    for _, duration := range time {
+        remainderFreq[duration % 60] += 1
+    }
+
+    for durationRemainder, freq := range remainderFreq {
+        if durationRemainder == 30 || durationRemainder == 0 {
+            res += freq * (freq - 1)
+        } else {
+            res += freq * remainderFreq[60 - durationRemainder]
+        }
+
+        
+    }
+
+    return int(res) / 2
+}
