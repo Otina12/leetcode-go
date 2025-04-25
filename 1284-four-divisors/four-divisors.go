ccf8eeb7d887e0fef0924c8a204ca5265b826b6d
Giorgi Otinashvili <130919363+Otina12@users.noreply.github.com>
2025-04-25 23:51:09 +0400

    Time: 10 ms (89.47%) | Memory: 8.1 MB (52.63%) - LeetSync

---
+++ 1284-four-divisors/four-divisors.go
@@ -0,0 +1,50 @@
+func sumFourDivisors(nums []int) int {
+    maxNum := 0
+    for _, num := range nums {
+        maxNum = max(maxNum, num)
+    }
+
+    isPrime := getPrimeNumbers(maxNum)
+    res := 0
+
+    for _, num := range nums {
+        numSqrt := int(math.Sqrt(float64(num)))
+
+        for x := 2; x <= numSqrt; x++ {
+            if !isPrime[x] {
+                continue
+            }
+            
+            if num % x == 0 {
+                quotient := num / x
+                
+                if quotient != x && (isPrime[quotient] || quotient == x*x) {
+                    res += 1 + x + quotient + num
+                    break
+                }
+            }
+        }
+    }
+
+    return res
+}
+
+func getPrimeNumbers(upper int) []bool {
+    isPrime := make([]bool, upper + 1)
+    
+    for i := 2; i <= upper; i++ {
+        isPrime[i] = true
+    }
+    
+    for num := 2; num <= int(math.Sqrt(float64(upper))); num++ {
+        if !isPrime[num] {
+            continue
+        }
+        
+        for x := num * num; x <= upper; x += num {
+            isPrime[x] = false
+        }
+    }
+    
+    return isPrime
+}
