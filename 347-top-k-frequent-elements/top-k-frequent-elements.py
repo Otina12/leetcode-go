481115326c001271edcfd94673aa62279567dc81
Giorgi Otinashvili <130919363+Otina12@users.noreply.github.com>
2025-04-26 22:02:48 +0400

    Time: 3 ms (91.01%) | Memory: 21.4 MB (40.28%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,6 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = [(k, val) for k, val in Counter(nums).items()]
-        freq.sort(key = lambda x : x[1])
+        freq = Counter(nums)
+        buckets = [[] for _ in range((max(freq.values()) + 1))]
 
-        return [key for (key, val) in freq[-k:]]
+        for key, val in freq.items():
+            buckets[val].append(key)
+
+        res = []
+
+        for i in range(len(buckets) - 1, -1, -1):
+            for num in buckets[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+
+        return []
