485d36b6f3edbedb6e886fb5ab33a830a7e1ca2c
Giorgi Otinashvili <130919363+Otina12@users.noreply.github.com>
2025-05-06 19:42:52 +0400

    Time: 0 ms (100.00%) | Memory: 3.9 MB (76.63%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.go
+++ 213-house-robber-ii/house-robber-ii.go
@@ -3,28 +3,19 @@ func rob(nums []int) int {
         return nums[0]
     }
     
-    lastHouse := nums[len(nums) - 1]
-    
-    nums = nums[:len(nums) - 1]
-    withoutLastHouse := robHelper(nums)
-
-    nums = append(nums, lastHouse)
-    nums = nums[1:]
-
-    withoutFirstHouse := robHelper(nums)
-
-    return max(withoutLastHouse, withoutFirstHouse)
+    return max(robHelper(nums, 1, len(nums) - 1), robHelper(nums, 0, len(nums) - 2))
 }
 
-func robHelper(nums []int) int {
-    if len(nums) == 1 {
-        return nums[0]
+func robHelper(nums []int, start int, end int) int {
+    if end == start {
+        return nums[start]
     }
+    
+    len := end - start + 1
+    prev, cur := 0, nums[start]
 
-    prev, cur := 0, nums[0]
-
-    for i := 1; i < len(nums); i++ {
-        next := max(cur, prev + nums[i])
+    for i := 1; i < len; i++ {
+        next := max(cur, prev + nums[start + i])
         prev = cur
         cur = next
     }
