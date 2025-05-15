40b19933e21e589e1a26d83bed7a28801348f483
Giorgi Otinashvili <130919363+Otina12@users.noreply.github.com>
2025-05-15 23:49:46 +0400

    Time: 0 ms (100.00%) | Memory: 5.9 MB (70.59%) - LeetSync

--- 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.go
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.go
@@ -1,22 +1,21 @@
 func getLongestSubsequence(words []string, groups []int) []string {
-    maxEndingAt := make([][]int, 2)
-    maxEndingAt[0] = []int{}
-    maxEndingAt[1] = []int{}
+    lastDigit := groups[0]
+    res := []string{words[0]}
 
-    for i := 0; i < len(groups); i++ {
-        maxEndingAt[groups[i]] = append(maxEndingAt[1-groups[i]], i)
-    }
-
-    var res []string
-    var maxI int = 0
-    
-    if len(maxEndingAt[1]) > len(maxEndingAt[0]) {
-        maxI = 1
-    }
-
-    for _, wordI := range maxEndingAt[maxI] {
-        res = append(res, words[wordI])
+    for i := 1; i < len(groups); i++ {
+        if abs(groups[i] - lastDigit) == 1 {
+            lastDigit = groups[i]
+            res = append(res, words[i])
+        }
     }
 
     return res
+}
+
+func abs(x int) int {
+    if x < 0 {
+        return -x
+    }
+
+    return x
 }
