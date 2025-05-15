669ca0e9a8de49a03e3c334a7f129ac08c697bc0
Giorgi Otinashvili <130919363+Otina12@users.noreply.github.com>
2025-05-15 23:35:17 +0400

    Time: 0 ms (100.00%) | Memory: 6 MB (20.59%) - LeetSync

--- 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.go
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.go
@@ -1,25 +1,21 @@
-type pair struct {
-    indices []int
-    length int
-}
-
 func getLongestSubsequence(words []string, groups []int) []string {
-    maxEndingAt := [2]pair{}
+    maxEndingAt := make([][]int, 2)
+    maxEndingAt[0] = []int{}
+    maxEndingAt[1] = []int{}
 
     for i := 0; i < len(groups); i++ {
-        maxEndingAt[groups[i]].indices = append(maxEndingAt[1-groups[i]].indices, i)
-        maxEndingAt[groups[i]].length = maxEndingAt[1-groups[i]].length + len(words[i])
+        maxEndingAt[groups[i]] = append(maxEndingAt[1-groups[i]], i)
     }
 
-    maxI := 0
-    if maxEndingAt[1].length > maxEndingAt[0].length {
+    var res []string
+    var maxI int = 0
+    
+    if len(maxEndingAt[1]) > len(maxEndingAt[0]) {
         maxI = 1
     }
 
-    res := []string{}
-
-    for _, index := range maxEndingAt[maxI].indices {
-        res = append(res, words[index])
+    for _, wordI := range maxEndingAt[maxI] {
+        res = append(res, words[wordI])
     }
 
     return res
