7f47ad3716b702779dc658dfe0435516512107db
Giorgi Otinashvili <130919363+Otina12@users.noreply.github.com>
2025-05-17 14:23:17 +0400

    Time: 0 ms (100.00%) | Memory: 15.6 MB (5.26%) - LeetSync

--- 134-gas-station/gas-station.go
+++ 134-gas-station/gas-station.go
@@ -1,18 +1,12 @@
 func canCompleteCircuit(gas []int, cost []int) int {
     n := len(gas)
-    changes := make([]int, n)
-
-    for i := 0; i < n; i++ {
-        changes[i] = gas[i] - cost[i]
-    }
-
     i, j := 0, n - 1
     totalGas := 0
 
     for i <= j {
-        totalGas += changes[i]
-        for i < j && totalGas < 0 {
-            totalGas += changes[j]
+        totalGas += gas[i] - cost[i]
+        for i <= j && totalGas < 0 {
+            totalGas += gas[j] - cost[j]
             j -= 1
         }
         i += 1
