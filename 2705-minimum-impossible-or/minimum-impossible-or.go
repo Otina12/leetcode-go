ff344fa9b38cbdf0ce27b9d6fd570a8a3a453a6f
Giorgi Otinashvili <130919363+Otina12@users.noreply.github.com>
2025-05-20 18:39:56 +0400

    Time: 35 ms (14.29%) | Memory: 11.1 MB (71.43%) - LeetSync

---
+++ 2705-minimum-impossible-or/minimum-impossible-or.go
@@ -0,0 +1,19 @@
+func minImpossibleOR(nums []int) int {
+    numSet := make(map[int]struct{})
+
+    for _, num := range nums {
+        numSet[num] = struct{}{}
+    }
+
+    x := 1
+
+    for {
+        if _, exists := numSet[x]; !exists {
+            break
+        } else {
+            x <<= 1
+        }
+    }
+
+    return x
+}
