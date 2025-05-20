e3ea95c2223dc64ffe87cafdff9d8a2e4484dfbd
Giorgi Otinashvili <130919363+Otina12@users.noreply.github.com>
2025-05-20 19:13:34 +0400

    Time: 6 ms (66.67%) | Memory: 12.8 MB (33.33%) - LeetSync

---
+++ 3141-minimum-size-subarray-in-infinite-array/minimum-size-subarray-in-infinite-array.go
@@ -0,0 +1,44 @@
+func minSizeSubarray(nums []int, target int) int {
+    n := len(nums)
+    nums2 := make([]int, 2*n)
+    totalSum := 0
+
+    for _, num := range nums {
+        totalSum += num
+    }
+
+    wholeArrCnt := target / totalSum
+    target %= totalSum
+
+    if target == 0 {
+        return n * wholeArrCnt
+    }
+
+    copy(nums2, nums)
+
+    for i := 0; i < n; i++ {
+        nums2[n+i] = nums[i]
+    }
+
+    curWindowSum := 0
+    left, res := 0, n
+
+    for right, num := range nums2 {
+        curWindowSum += num
+
+        for left < right && curWindowSum > target {
+            curWindowSum -= nums2[left]
+            left += 1
+        }
+
+        if curWindowSum == target {
+            res = min(res, right - left + 1)
+        }
+    }
+
+    if res == n {
+        return -1
+    }
+
+    return res + n * wholeArrCnt
+}
